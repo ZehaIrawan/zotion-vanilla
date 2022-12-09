f533094a7ab8bd61f38a62ac26f34939ecbca37a
Zeha Irawan <zehairawan@gmail.com>
2022-12-09 17:12:06 +0800

    create new block

--- editablePage.js
+++ editablePage.js
@@ -11,14 +11,44 @@ export function editablePage() {
     },
   ];
 
+  const keydownHandler = (e) => {
+    console.log(`${e.key} pressed`)
+    if (e.key === "Enter") {
+      e.preventDefault();
+      handleCreateBlock();
+    }
+    if (e.key === "/") {
+      alert('show command')
+    }
+    // arrow up => move to previous block
+    // arrow down => move to next block
+    // backspace => delete block
+  }
+
+  const handleCreateBlock = () => {
+    console.log('create new block')
+    defaultBlocks.push({
+      tag: "div",
+      text: "Type &#47 for commands",
+      id: uuidv4(),
+    });
+
+    const lastBlock = defaultBlocks[defaultBlocks.length - 1];
+     const node = document.createElement(lastBlock.tag);
+     node.setAttribute("id", lastBlock.id);
+     node.addEventListener("keydown", keydownHandler);
+     node.setAttribute("contentEditable", "true");
+     node.setAttribute("placeholder", lastBlock.text);
+     document.getElementById("editablePage").appendChild(node);
+     node.focus()
+  }
 
   defaultBlocks.map(block => {
     const node = document.createElement(block.tag);
     node.setAttribute("id", block.id);
+    node.addEventListener("keydown", keydownHandler);
     node.setAttribute("contentEditable", "true");
     node.setAttribute("placeholder", block.text);
-    // const textnode = document.createTextNode(block.text);
-    // node.appendChild(textnode);
     document.getElementById("editablePage").appendChild(node);
   })
 
